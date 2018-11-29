92fcf1682fcca18b7fac178b2210ef0957b54dc3
Nothando <37573058+Nothandol@users.noreply.github.com>
2018-11-29 16:22:44 +0200

    Add files via upload

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/uploads';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
